afc45f08054b0dce20b6a4446c6f1f9a752db9e0
gustavo ferreira <gstvmorinico@gmail.com>
2024-10-20 18:43:09 -0300

    correçoes gerais

--- src/app/components/cardapio/cardapio.component.ts
+++ src/app/components/cardapio/cardapio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AppComponent } from "../../app.component";
 
 @Component({
   selector: 'app-cardapio',
   standalone: true,
-  imports: [],
+  imports: [AppComponent],
   templateUrl: './cardapio.component.html',
   styleUrl: './cardapio.component.css'
 })
